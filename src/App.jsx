75dd6e9075e5a2c52382298eb13d91db9b65016c
Jaskirat257 <kaurjaskirat990@gmail.com>
2025-10-22 22:53:08 -0400

    Project-1 Grocery

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import GroceriesAppContainer from "./components/GroceriesAppContainer.jsx";
 
+// Author: Jaskirat
+// Description: Project-1
+
 function App() {
   return <GroceriesAppContainer />;
 }
-
 
 
 export default App;
